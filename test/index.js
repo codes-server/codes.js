2f439b6b50fcf05880d128acc467eac029449bcc
Abady <gamersspeaks@gmail.com>
2019-01-20 21:16:13 +0300

    chore: added tests

---
+++ test/index.js
@@ -0,0 +1,9 @@
+const { TOKEN } = require('./auth');
+const { Client } = require('../src/index');
+const client = new Client(TOKEN, '171259176029257728');
+client.start();
+client.on('ready', () => {
+/* eslint-disable no-console */
+  console.log('ready');
+  console.log(client.owner);
+});
