e9b3323a527999b4af5cc80cb43ced19f6c5f9a4
aboodxgameing <aboodxgameing@gmail.com>
2019-01-21 07:15:10 -0500

    Edit Bugs

--- src/client/CodesClient.js
+++ src/client/CodesClient.js
@@ -1,19 +1,20 @@
 const { Client } = require('discord.js');
 var client = new Client()
 class CodesClient extends Client {
-  constructor(token, owner) {
+  constructor(token, owner, prefix) {
     super({
       messageCacheMaxSize: 1000,
     });
     this.token = token;
     this.owner = owner;
+    this.prefix = prefix
   }
   start() {
     this.login(this.token);
   }
   command(name, settings, cmd) {
     client.on("message", message => {
-      if(message.content === prefix + name){
+      if(message.content === this.prefix + name){
       cmd(message)
       }
     });
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const { TOKEN } = require('./auth');
 const { Client } = require('../src/index');
-const client = new Client(TOKEN, '171259176029257728');
+const client = new Client(TOKEN, '171259176029257728', "-");
 client.start();
 client.on('ready', () => {
 /* eslint-disable no-console */
@@ -9,4 +9,4 @@ client.on('ready', () => {
 });
 client.command("ping", {aliases: ["pings", "pong"]}, function cmd(message) {
   message.channel.send("PONG!")
-})
+})  
