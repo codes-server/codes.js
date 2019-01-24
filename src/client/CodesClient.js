944576dced4aa8c9e2ecf9a7599fbb572b889f02
rokzartz <aboodxgameing@gmail.com>
2019-01-24 16:13:34 +0300

    ..

--- src/client/CodesClient.js
+++ src/client/CodesClient.js
@@ -5,7 +5,7 @@ class CodesClient extends Client {
     super({
       messageCacheMaxSize: 1000,
     });
-    // Let command_path = path.commands
+    // let command_path = path.commands
     // this.on('message', message => {
     //   if (message.content.indexOf(this.prefix) !== 0) return;
     //   const args = message.content.slice(this.prefix.length).trim().split(/ +/g);
@@ -19,23 +19,17 @@ class CodesClient extends Client {
   }
 
   command(settings, exec) {
-<<<<<<< HEAD
     let aliases = settings.aliases
     let name = settings.name
     let perm = settings.permissions;
     let roles = settings.roles;
-
+    let ownerOnly = settings.ownerOnly;
+    
     if(!name) return;
     if(!aliases) aliases = []
     if(!perm) perm = []
     if(!roles) roles = []
-=======
-    this.settings = settings;
-    let aliases = this.settings.aliases || [];
-    let name = this.settings.name;
-    if (!name || !exec) return;
-
->>>>>>> 4e90daa7516f19b744b7f18fc00f8f65c4f53367
+    if(!ownerOnly) ownerOnly = false
     this.on('message', message => {
       if (message.content.indexOf(this.prefix) !== 0) return;
       const args = message.content.slice(this.prefix.length).trim().split(/ +/g);
@@ -44,6 +38,7 @@ class CodesClient extends Client {
         exec(message);
       }
     });
+    return undefined;
   }
 
 
