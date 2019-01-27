d38954fda06809b85c167e6c7c67a54b506eae10
NourEldien <38608933+NourEldienHE@users.noreply.github.com>
2019-01-27 17:12:48 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,20 +1,42 @@
-const { TOKEN } = require('../../auth');
+const { TOKEN } = require('./auth');
 const { Client, Plugins } = require('../src/index');
-const client = new Client(TOKEN, ['228401267263668224'], '-');
+const client = new Client(TOKEN, ['228401267263668224','407595276946243604','354716386716811264','171259176029257728','228401267263668224'], '-');
+//new Client(توكن بوتك, ['YOUR ID'], 'بريفكس بوتك')
 client.start();
 client.on('ready', () => {
-/* eslint-disable no-console */
-  console.log('ready');
-  console.log(client.owner);
-  console.log(client.user.tag);
+  /* eslint-disable no-console */
+  console.log(`${client.user.tag} is ready
+OWNER: ${client.owner}`);
 });
-client.command({name: "ping", aliases: ['kpop', 'brok', 'pings', 'pong'], ownerOnly: true}, message => {
-  message.channel.send('PONG!');
-});
-client.command({name: "members", aliases: ["users", "member"]}, msg => {
-Plugins.members(msg)
-})
-client.command({name: "help"}, message => {
-  message.channel.send("Send")
-})
+client.command({
+    name: "ping",
+    aliases: ['kpop', 'brok', 'pings', 'pong']
+  },
+  message => {
+    message.channel.send('PONG!');
+  });
 
+  client.command({
+    name: "status",
+    aliases: ['status','bot-status'],
+    ownerOnly: true
+  }, message=>{
+    message.channel.send(`CHANGE BOT STATUS...`)
+  });
+client.command({
+  name: ""
+})
+client.command({
+    name: "members",
+    aliases: ["users", "member", "الأعضاء"]
+  },
+  msg => {
+    Plugins.members(msg)
+  })
+
+client.command({
+  name: "help",
+  aliases: ["help", "مساعدة", "قائمة الأوامر", "commands"]
+}, message => {
+  message.channel.send(client.user.username + " Help list:\n")
+})
