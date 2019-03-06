d796437325f21da0142c9510e97ffa3069743c80
Abady <gamersspeaks@gmail.com>
2019-03-06 20:22:32 +0300

    fix: delete non-planned things yet

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ module.exports = {
   // Core
   Client: require('./client/CodesClient'),
   Plugins: require('./plugins/index'),
+
+  // Version
+  version: require('../package.json').version,
 };
--- test/index.js
+++ test/index.js
@@ -1,12 +1,13 @@
 const { TOKEN } = require('../../auth');
-const { Client, Plugins } = require('../src/index');
+const { Client, Plugins, version } = require('../src/index');
 const client = new Client(TOKEN, ['228401267263668224','407595276946243604','354716386716811264','171259176029257728','298732816995319809'], '-');
-//new Client(توكن بوتك, ['YOUR ID'], 'بريفكس بوتك')
-client.start();
+
 client.on('ready', () => {
   /* eslint-disable no-console */
   console.log(`${client.user.tag} is ready
-OWNER: ${client.owner}`);
+OWNER: ${client.owner}
+VERSION: ${version}
+`);
 });
 client.command({
     name: "ping",
@@ -46,3 +47,5 @@ client.command({
 }, message => {
   message.channel.send(client.user.username + " Help list:\n")
 })
+
+client.start();
